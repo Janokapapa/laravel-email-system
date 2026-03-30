58fad903e13d026d9ceb15f8490b6f3751231773
Jan <jan@jandev.eu>
2026-03-30 11:43:55 +0700

    fix text part: resolve unsubscribe placeholders, decode entities, add newlines

--- src/Support/PmtaSpooler.php
+++ src/Support/PmtaSpooler.php
@@ -168,7 +168,20 @@ EOT;
             }
 
             $htmlBody = quoted_printable_encode($rawHtml);
-            $textBody = quoted_printable_encode(trim(strip_tags((string) $emailLog->message)));
+
+            // Generate plain text part: resolve unsubscribe placeholders as URLs,
+            // convert block elements to newlines, strip tags, decode HTML entities.
+            $rawText = (string) $emailLog->message;
+            if ($unsubscribeUrl) {
+                $rawText = preg_replace('/\{\{unsubscribe=(.+?)\}\}/', '$1: ' . $unsubscribeUrl, $rawText);
+                $rawText = str_replace('{{unsubscribe_url}}', $unsubscribeUrl, $rawText);
+            } else {
+                $rawText = self::stripUnsubscribePlaceholders($rawText);
+            }
+            $rawText = preg_replace('/<br\s*\/?>/i', "\n", $rawText);
+            $rawText = preg_replace('/<\/(p|div|h[1-6])>/i', "\n\n", $rawText);
+            $rawText = html_entity_decode(strip_tags($rawText), ENT_QUOTES, 'UTF-8');
+            $textBody = quoted_printable_encode(trim($rawText));
 
             $eml = <<<EOT
 x-sender: {$envelopeSender}
